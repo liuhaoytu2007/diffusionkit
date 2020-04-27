47e694278795d537993934bb7fa0e3abde7711e1
liuhaoytu2007 <liuhaoytu2007@126.com>
2020-04-27 16:18:56 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -18,7 +18,7 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('./source/exts/toggle'))
 #sys.path.insert(0, os.path.abspath('./exts/toggle'))
-sys.path.append(os.path.abspath('./source/exts/toggle'))
+sys.path.append(os.path.abspath('source/exts/toggle'))
 
 # -- General configuration -----------------------------------------------------
 
